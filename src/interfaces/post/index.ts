5816d14b0bee6726484f94ee3ffc279c97019271
roq-ai <>
2023-07-02 02:18:13 +0000

    Initial commit

---
+++ src/interfaces/post/index.ts
@@ -0,0 +1,26 @@
+import { CommentInterface } from 'interfaces/comment';
+import { UserInterface } from 'interfaces/user';
+import { OrganizationInterface } from 'interfaces/organization';
+import { GetQueryInterface } from 'interfaces';
+
+export interface PostInterface {
+  id?: string;
+  content: string;
+  user_id?: string;
+  organization_id?: string;
+  created_at?: any;
+  updated_at?: any;
+  comment?: CommentInterface[];
+  user?: UserInterface;
+  organization?: OrganizationInterface;
+  _count?: {
+    comment?: number;
+  };
+}
+
+export interface PostGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  content?: string;
+  user_id?: string;
+  organization_id?: string;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,121 @@
+import { Button, Flex, Heading, Image, Text, Stack, useBreakpointValue, Box, Link } from '@chakra-ui/react';
+
+import { signIn, signUp, requireNextAuth } from '@roq/nextjs';
+
+import Head from 'next/head';
+import { HelpBox } from 'components/help-box';
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>{`Rude`}</title>
+
+        <meta
+          name="description"
+          content="Experience Rude, your new favorite microblogging platform. Connect, share, and discover in a space designed for dynamic conversations."
+        />
+      </Head>
+
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+          <Stack position="relative" spacing={6} w={'full'} maxW={'lg'}>
+            <HelpBox />
+            <Image src="/roq.svg" alt="Logo" w="150px" mb="8" />
+            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+              <Text as={'span'}>Explore</Text>{' '}
+              <Text
+                as={'span'}
+                position={'relative'}
+                _after={{
+                  content: "''",
+                  width: 'full',
+                  height: useBreakpointValue({ base: '20%', md: '30%' }),
+                  position: 'absolute',
+                  bottom: 1,
+                  left: 0,
+                  bg: 'cyan.400',
+                  zIndex: -1,
+                }}
+              >
+                {`Rude`}
+              </Text>
+            </Heading>
+            <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+              {`Experience Rude, your new favorite microblogging platform. Connect, share, and discover in a space designed for dynamic conversations.`}
+            </Text>
+            <Stack direction="column" spacing={4} className="roles-container">
+              <Text>Organization</Text>
+              <Stack className="owner-roles-container" direction={{ base: 'column', md: 'row' }} spacing={4}>
+                <Button
+                  rounded={'full'}
+                  bg={'cyan.500'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'cyan.700',
+                  }}
+                  onClick={() => signUp('administrator')}
+                >
+                  Create Account
+                </Button>
+                <Button rounded={'full'} onClick={() => signIn('administrator')}>
+                  Login
+                </Button>
+              </Stack>
+
+              <Text>End User</Text>
+              <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+                <Button
+                  rounded={'full'}
+                  bg={'cyan.500'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'cyan.700',
+                  }}
+                  onClick={() => signUp('end-user')}
+                >
+                  Register
+                </Button>
+                <Button rounded={'full'} onClick={() => signIn('end-user')}>
+                  Login
+                </Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex position="relative" flex={1}>
+          <Image
+            src={
+              'https://images.unsplash.com/photo-1491438590914-bc09fcaaf77a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NjA3NjB8MHwxfHNlYXJjaHwxfHxtaWNyb2Jsb2dnaW5nJTJDc29jaWFsJTIwbWVkaWF8ZW58MHx8fHwxNjg4MjY0MjE3fDA&ixlib=rb-4.0.3&q=80&w=1080'
+            }
+            alt={'Login Image'}
+            objectFit={'cover'}
+          />
+          <Box position="absolute" top="0" backgroundColor="rgba(0,0,0,0.6)" width="100%" py="2">
+            <Text align="center" fontSize="sm" color="white">
+              Photo by{' '}
+              <Link
+                href="https://unsplash.com/es/@priscilladupreez?utm_source=roq-generator&utm_medium=referral"
+                isExternal
+                color="teal.200"
+              >{`Priscilla Du Preez`}</Link>{' '}
+              on{' '}
+              <Link
+                href="https://unsplash.com/?utm_source=roq-generator&utm_medium=referral"
+                isExternal
+                color="teal.200"
+              >
+                Unsplash
+              </Link>
+            </Text>
+          </Box>
+        </Flex>
+      </Stack>
+    </>
+  );
+}
+
+export default requireNextAuth({
+  redirectIfAuthenticated: true,
+  redirectTo: '/users',
+})(HomePage);
